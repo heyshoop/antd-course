2a7af070532742f237276abfb908d9d11c12ec46
anchor <netuser.orz@icloud.com>
2018-12-21 15:16:01 +0800

    尝试各种组件

--- src/page/HelloWorld.js
+++ src/page/HelloWorld.js
@@ -1,3 +1,21 @@
-export default () => {
-    return <div>hello world</div>;
+import React from 'react';
+import Content from './Content';
+import Picture from './Picture';
+import Square from './Square';
+
+class helloworld extends React.Component{
+    render(){
+        const picture = {
+            src:'https://cdn.nlark.com/yuque/0/2018/jpeg/84141/1536207007004-59352a41-4ad8-409b-a416-a4f324eb6d0b.jpeg',
+        };
+        return(
+            <div className='container'>
+            <Content/>
+            <Picture src={picture.src}>
+            // 这里放置的内容就是 props.children
+            </Picture><Square/>
+            </div>
+        )
+    }
 }
+export default helloworld;
