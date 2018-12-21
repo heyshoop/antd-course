b8b196577a445f8fc94592965bfb6b829adaa860
anchor <netuser.orz@icloud.com>
2018-12-21 22:43:03 +0800

    尝试第一个antd控件

--- src/page/HelloWorld.js
+++ src/page/HelloWorld.js
@@ -1,20 +1,16 @@
 import React from 'react';
-import Content from './Content';
-import Picture from './Picture';
-import Square from './Square';
+import Card from './Card';
 
 class helloworld extends React.Component{
     render(){
-        const picture = {
-            src:'https://cdn.nlark.com/yuque/0/2018/jpeg/84141/1536207007004-59352a41-4ad8-409b-a416-a4f324eb6d0b.jpeg',
+        const style = {
+            width:'400px',
+            margin:'30px',
+            boxShadow:'0 4px 8px 0 rgba(0,0,0,0.2)',
+            border:'1px solid #e8e8e8',
         };
         return(
-            <div className='container'>
-            <Content/>
-            <Picture src={picture.src}>
-            // 这里放置的内容就是 props.children
-            </Picture><Square/>
-            </div>
+            <Card/>
         )
     }
 }
